8af69aede9f5e39754244d88e57730eb8f9bffd0
rtoshkov <rosentoshkov@gmail.com>
2023-01-07 15:07:28 +0200

    started work on Inventory form

--- application/src/components/Inventory/Inventory.js
+++ application/src/components/Inventory/Inventory.js
@@ -1,9 +1,50 @@
+import {sheetAddress, sheetNames} from '../Utils/InventoryFileStructure'
+
 const Inventory = (props) => {
 
+    function inventoryHandler(){
+        //pass
+    }
+
+    function handleCheckbox(){
+
+    }
+
+    const sheetAddressKeys = Object.keys(sheetAddress);
+
     return (
-        <ul>
-            {props.results.map((result) => <li key={result}>{result}</li>)}
-        </ul>
+        <>
+        <form onSubmit={inventoryHandler}>
+            <label htmlFor="sheetAddress">Which file? </label>
+            <select id="sheetAddress" name="sheetAddress">
+                {
+                sheetAddressKeys.map((k) => (
+                 <option key={k} value={sheetAddress[k]}>{k}</option>
+            ))
+                }
+            </select>
+
+            <label htmlFor="sheetName">What equipment? </label>
+            <select id="sheetName" name="sheetName">
+                {
+                    sheetNames.map((name) => (
+                        <option key={name} value={name}>{name}</option>
+                    ))
+                }
+            </select>
+
+            <label htmlFor="save">Добави в колона Serials# ако липсва от там</label>
+              <input onChange={handleCheckbox} type="checkbox" id="save" name="save" value="true"/>
+
+
+
+
+        </form>
+
+        {/*<ul>*/}
+        {/*    {props.results.map((result) => <li key={result}>{result}</li>)}*/}
+        {/*</ul>*/}
+        </>
     )
 }
 
---
+++ application/src/components/Utils/InventoryFileStructure.js
@@ -0,0 +1,19 @@
+const sheetAddress = {
+    'Telus Inventory': 'TEST_TELUS_INVENTORY',
+    'Infinity Inventory': 'TEST_INFINITY_INVENTORY',
+    'Plovdiv Inventory':'TEST_PLOVDIV_INVENTORY'
+}
+
+const sheetNames = [
+    'Normal PCs',
+    'Gaming PCs',
+    'ThinClients',
+    'ChromeBOXES',
+    'ChromeBOOKS',
+    'Monitors'
+]
+
+module.exports = {
+    sheetAddress,
+    sheetNames,
+}
